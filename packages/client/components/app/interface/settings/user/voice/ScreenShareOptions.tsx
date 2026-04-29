bded82d2863ac3e70ff679276e4e9294836f8032
Stranik2504 <58374405+Stranik2504@users.noreply.github.com>
2026-04-29 21:49:39 +0300

    fix: conditionally render screen share settings based on voice context

--- packages/client/components/app/interface/settings/user/voice/ScreenShareOptions.tsx
+++ packages/client/components/app/interface/settings/user/voice/ScreenShareOptions.tsx
@@ -1,3 +1,4 @@
+import { Show, createMemo } from "solid-js";
 import { Trans } from "@lingui-solid/solid/macro";
 
 import { useVoice } from "@revolt/rtc";
@@ -16,40 +17,45 @@ export function ScreenShareOptions() {
   const { voice } = useState();
   const voiceContext = useVoice();
 
-  const qualities = voiceContext.getEnabledScreenShareQualities();
+  const qualities = createMemo(() => {
+    if (!voiceContext) return {};
+    return voiceContext.getEnabledScreenShareQualities();
+  });
 
   return (
-    <Column>
-      <Text class="title">
-        <Trans>Screen Share Settings</Trans>
-      </Text>
-      <CategoryButton.Group>
-        <CategoryButton.Select
-          icon={<Symbol>screen_share</Symbol>}
-          title={<Trans>Select screen share quality</Trans>}
-          options={
-            Object.fromEntries(
-              Object.keys(qualities).map((name) => [
-                name,
-                {
-                  title: qualities[name as ScreenShareQualityName]!.fullName,
-                },
-              ]),
-            ) as { [key in ScreenShareQualityName]: CategorySelectOption }
-          }
-          value={voice.screenShareQuality}
-          onUpdate={(ns) => (voice.screenShareQuality = ns)}
-        />
-        <CategoryButton
-          icon="blank"
-          action={<Checkbox checked={voice.screenShareQualityAsk} />}
-          onClick={() =>
-            (voice.screenShareQualityAsk = !voice.screenShareQualityAsk)
-          }
-        >
-          <Trans>Always Ask for Screen Share Quality</Trans>
-        </CategoryButton>
-      </CategoryButton.Group>
-    </Column>
+    <Show when={voiceContext && Object.keys(qualities()).length > 0}>
+      <Column>
+        <Text class="title">
+          <Trans>Screen Share Settings</Trans>
+        </Text>
+        <CategoryButton.Group>
+          <CategoryButton.Select
+            icon={<Symbol>screen_share</Symbol>}
+            title={<Trans>Select screen share quality</Trans>}
+            options={
+              Object.fromEntries(
+                Object.keys(qualities()).map((name) => [
+                  name,
+                  {
+                    title: qualities()[name as ScreenShareQualityName]!.fullName,
+                  },
+                ]),
+              ) as { [key in ScreenShareQualityName]: CategorySelectOption }
+            }
+            value={voice.screenShareQuality}
+            onUpdate={(ns) => (voice.screenShareQuality = ns)}
+          />
+          <CategoryButton
+            icon="blank"
+            action={<Checkbox checked={voice.screenShareQualityAsk} />}
+            onClick={() =>
+              (voice.screenShareQualityAsk = !voice.screenShareQualityAsk)
+            }
+          >
+            <Trans>Always Ask for Screen Share Quality</Trans>
+          </CategoryButton>
+        </CategoryButton.Group>
+      </Column>
+    </Show>
   );
 }
